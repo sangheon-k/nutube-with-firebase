7d0c0a9579eec60c71cfc124ca2c0e507019b4c2
sangheon-k <sangheon.k@kakao.com>
2023-10-15 20:11:59 +0900

    feat : HomePage Use Custom Hook

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,73 +1,18 @@
-import { useEffect, useState } from 'react';
-import { Unsubscribe } from 'firebase/auth';
-import {
-  collection,
-  limit,
-  onSnapshot,
-  orderBy,
-  query,
-} from 'firebase/firestore';
 import { db } from '../../../firebase';
-import { IVideo } from '@/types';
 import VideoCard from '../VideoCardList/VideoCard';
+import useGetSnapshot from '@/hooks/useGetSnapshot';
+import { collection, limit, orderBy, query } from 'firebase/firestore';
+import { IVideo } from '@/types';
 
 const HomePage = () => {
-  const [videos, setVideos] = useState<IVideo[]>([]);
-  useEffect(() => {
-    let unsubscribe: Unsubscribe | null = null;
-    const fetchVideos = async () => {
-      const videosQuery = query(
-        collection(db, 'videos'), // collection
-        orderBy('createdAt', 'desc'), // orderBy
-        limit(25), // limit
-      );
-      unsubscribe = await onSnapshot(videosQuery, (snapshot) => {
-        const videos = snapshot.docs.map((doc) => {
-          const {
-            views,
-            writer,
-            writerId,
-            title,
-            description,
-            privacy,
-            filePath,
-            category,
-            duration,
-            thumbnail,
-            createdAt,
-            updatedAt,
-          } = doc.data();
-          return {
-            id: doc.id,
-            views,
-            writer,
-            writerId,
-            title,
-            description,
-            privacy,
-            filePath,
-            category,
-            duration,
-            thumbnail,
-            createdAt,
-            updatedAt,
-          };
-        });
-        setVideos(videos);
-      });
-    };
-    fetchVideos();
-    return () => {
-      unsubscribe && unsubscribe();
-    };
-  }, []);
-
-  console.log(videos);
+  const { data: videos } = useGetSnapshot(
+    query(collection(db, 'videos'), orderBy('createdAt', 'desc'), limit(25)),
+  );
 
   return (
     <main className="p-10 overflow-y-auto">
       <div className="grid mx-auto sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-        {videos.map((video) => (
+        {videos.map((video: IVideo) => (
           <VideoCard key={video.id} video={video} />
         ))}
       </div>
